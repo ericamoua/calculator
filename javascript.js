5db307abf1c9d31382a768df1484299fb01e5b70
EricaMoua <erica.moua568@gmail.com>
2024-04-29 22:54:23 -0400

    pseudo code

---
+++ javascript.js
@@ -0,0 +1,41 @@
+const number = Number("1" + "2") + 1;
+console.log(number);
+
+/**
+ * when you press a button, you get a string?
+ * sting + string + string = stringstringstring
+ * 
+ * so that means, '1' + '2' + '3' = '123'
+ * 
+ * Number('123') -> 123
+ * 
+ * 123 ??? 123 = ???
+ * 
+ * 1. entering a string
+ * 2. after clicking an operator your first string turns into a number
+ * 3. you start your 2nd string
+ * 4. after you press enter the 2nd string turns into a number
+ * 5. now you do your logic
+ * 
+ * Number(??) + Number(??) = ???
+ * 
+ * 1stString + 2ndString = ???
+ * 
+ * first step in the logic
+ * *creat the first string
+ * --when you press your #'s you put them together
+ */
+
+let num1 = '';
+let num2 = '';
+//empty string because we don't know the number 
+
+num1 + what ever the user presses = '' + '' = ''
+
+//we need a Number not a string
+//So answer will always be a Number
+// answer = Number(num1) + Number(num2)
+const answer;
+
+
+//write down this the process and if i get stuck stop research and get back to it 
