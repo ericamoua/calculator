53a8bdeb6921346d6aabd3ad61774aad2e65af69
EricaMoua <erica.moua568@gmail.com>
2024-05-07 10:53:52 -0400

    final

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-const number = Number("1" + "2") + 1;
-console.log(number);
 
 /**
  * when you press a button, you get a string?
@@ -25,17 +23,197 @@ console.log(number);
  * *creat the first string
  * --when you press your #'s you put them together
  */
-
+{
 let num1 = '';
 let num2 = '';
-//empty string because we don't know the number 
+let ops = '';
+const audio = document.getElementById("dabinSound");
 
-num1 + what ever the user presses = '' + '' = ''
+function updateDisplay() {
+    document.getElementById('display').value = num1 + ops + num2;
+    audio.play();
+}
 
-//we need a Number not a string
-//So answer will always be a Number
-// answer = Number(num1) + Number(num2)
-const answer;
+document.getElementById('btn1').addEventListener('click', () => {
+    if (ops === '') {
+        num1 += '1';
+    } else {
+        num2 += '1';
+    }
+    updateDisplay();
+});
+
+document.getElementById('btn2').addEventListener('click', () => {
+    if (ops === '') {
+        num1 += '2';
+    } else {
+        num2 += '2';
+    }
+    updateDisplay();
+});
+document.getElementById('btn3').addEventListener('click', () => {
+    if (ops === '') {
+        num1 += '3';
+    } else {
+        num2 += '3';
+    }
+    updateDisplay();
+});
+document.getElementById('btn4').addEventListener('click', () => {
+    if (ops === '') {
+        num1 += '4';
+    } else {
+        num2 += '4';
+    }
+    updateDisplay();
+});
+document.getElementById('btn5').addEventListener('click', () => {
+    if (ops === '') {
+        num1 += '5';
+    } else {
+        num2 += '5';
+    }
+    updateDisplay();
+});
+document.getElementById('btn6').addEventListener('click', () => {
+    if (ops === '') {
+        num1 += '6';
+    } else {
+        num2 += '6';
+    }
+    updateDisplay();
+});
+document.getElementById('btn7').addEventListener('click', () => {
+    if (ops === '') {
+        num1 += '7';
+    } else {
+        num2 += '7';
+    }
+    updateDisplay();
+});
+document.getElementById('btn8').addEventListener('click', () => {
+    if (ops === '') {
+        num1 += '8';
+    } else {
+        num2 += '8';
+    }
+    updateDisplay();
+});
+document.getElementById('btn9').addEventListener('click', () => {
+    if (ops === '') {
+        num1 += '9';
+    } else {
+        num2 += '9';
+    }
+    updateDisplay();
+});
+
+// Event listener for Clear Button
+document.getElementById('clear').addEventListener('click', () => {
+    num1 = '';
+    num2 = '';
+    ops = '';
+    display.value = '';
+});
+
+// Event listener for Delete Button
+document.getElementById('delete').addEventListener('click', () => {
+    if (ops === '') {
+        num1 = num1.slice(0, -1);
+    } else {
+        num2 = num2.slice(0, -1);
+    }
+    updateDisplay();
+});
+
+// Event listener for Percentage Button
+document.getElementById('percentage').addEventListener('click', () => {
+    if (ops === '') {
+        num1 = (parseFloat(num1) / 100).toString();
+    } else {
+        num2 = (parseFloat(num2) / 100).toString();
+    }
+    updateDisplay();
+});
+// Event listener for Minus Button
+document.getElementById('minus').addEventListener('click', () => {
+    ops = '-';
+    updateDisplay();
+});
+
+// Event listener for Add Button
+document.getElementById('add').addEventListener('click', () => {
+    ops = '+';
+    updateDisplay();
+});
+
+document.getElementById('btn0').addEventListener('click', () => {
+    if (ops === '') {
+        num1 += '0';
+    } else {
+        num2 += '0';
+    }
+    updateDisplay();
+});
 
 
-//write down this the process and if i get stuck stop research and get back to it 
+document.getElementById('decimal').addEventListener('click', () => {
+    if (ops === '') {
+        if (!(num1.includes('.'))) {
+            num1 += '.';
+            console.log(num1);
+        }
+    } else {
+       if (!(num2.includes('.'))) {
+        num2 += '.';
+        console.log(num2);
+       }
+    }
+    updateDisplay();
+});
+//we are trying to find out if we have more than one or two decimals in one string
+// Event listener for Multiply Button
+document.getElementById('multiply').addEventListener('click', () => {
+    ops = '*';
+    updateDisplay();
+});
+
+// Event listener for Divide Button
+document.getElementById('divide').addEventListener('click', () => {
+    ops = '/';
+    updateDisplay();
+});
+
+// Event listener for Equals Button
+document.getElementById('equals').addEventListener('click', () => {
+    if (num1 !== '' && num2 !== '') {
+        let result;
+        switch (ops) {
+            case '+':
+                result = parseFloat(num1) + parseFloat(num2);
+                break;
+            case '-':
+                result = parseFloat(num1) - parseFloat(num2);
+                break;
+            case '*':
+                result = parseFloat(num1) * parseFloat(num2);
+                break;
+            case '/':
+                if (parseFloat(num2) !== 0) {
+                    result = parseFloat(num1) / parseFloat(num2);
+                } else {
+                    result = 'Error: Divide by zero';
+                }
+                break;
+            default:
+                result = 'Error';
+        }
+        display.value = result;
+        num1 = result.toString();
+        num2 = '';
+        ops = '';
+    }
+});
+}
+
+
